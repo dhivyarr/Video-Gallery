3c31603a02b3e1d47c0213032dddae47d983a095
dhivyarr <136148761+dhivyarr@users.noreply.github.com>
2023-07-20 12:31:29 +0530

    add folder

---
+++ Video_Gallery_Project/video_project_1/src/App.js
@@ -0,0 +1,17 @@
+// import logo from './logo.svg';
+import './App.css';
+import  VideoTemplates from './VideoTemplates'
+
+function App() {
+  return (
+    
+    <div className="App">
+      <header class='header'><h1>Video Gallery</h1></header>
+      <VideoTemplates/>
+
+
+    </div>
+  );
+}
+
+export default App;
