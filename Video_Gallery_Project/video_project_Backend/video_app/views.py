c9c3884ccd5d9cb9f008e46763074fa8171552ff
Mounika Kema <mounika.k@tmachinesoftware.com>
2023-08-30 15:33:34 +0530

    i changed 3rd line
    Signed-off-by: Mounika Kema <mounika.k@tmachinesoftware.com>

--- Video_Gallery_Project/video_project_Backend/video_app/views.py
+++ Video_Gallery_Project/video_project_Backend/video_app/views.py
@@ -1,6 +1,6 @@
 from rest_framework import generics
 from .models import Video
-from .serializers import VideoSerializer
+from .serializers import Video
 
 class VideoListCreateView(generics.ListCreateAPIView):
     queryset = Video.objects.all()
