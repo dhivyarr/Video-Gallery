5e6f764f44aec916baa9bf4862e3aabb8df8c677
Mounika Kema <mounika.k@tmachinesoftware.com>
2023-08-31 11:25:12 +0530

    wertyui
    Signed-off-by: Mounika Kema <mounika.k@tmachinesoftware.com>

--- Video_Gallery_Project/video_project_Backend/MOUNI.py
+++ Video_Gallery_Project/video_project_Backend/MOUNI.py
@@ -19,9 +19,8 @@ qwertyuiopasdfghjklzxcvbnm,wertyiopsdfvbnm wertcbb g vhdts fdfdxgv bn mn  j hu g
 qwertyuiopasdfghjklzxcvbnm,wertyiopsdfvbnm wertcbb g vhdts fdfdxgv bn mn  j hu gguguguygug  uiuiuuguyfd  huuyuyyyuyuyug jhhvudrseewwuio gghxdxdfdfdghj ghdfdsdsffgfcbjhhghgfxfdxdfxfxcccgcfgxgfx ghhvcxsdrtyujvvnk vccxddkhkjhmn cf mjiuyrdvkhgutyrtwvhgjhlkjjhlkjhasaaaaaaaaaaaaaasdfgh jkkhgfghjkl
 qwertyuiopasdfghjklzxcvbnm,wertyiopsdfvbnm wertcbb g vhdts fdfdxgv bn mn  j hu gguguguygug  uiuiuuguyfd  huuyuyyyuyuyug jhhvudrseewwuio gghxdxdfdfdghj ghdfdsdsffgfcbjhhghgfxfdxdfxfxcccgcfgxgfx ghhvcxsdrtyujvvnk vccxddkhkjhmn cf mjiuyrdvkhgutyrtwvhgjhlkjjhlkjhasaaaaaaaaaaaaaasdfgh jkkhgfghjkl
 
-<<<<<<< HEAD
 Birds are animals that have feathers, wings, and beaks. They are warm-blooded and lay eggs. Birds are often found in the sky, trees, and on the ground, and they come in various shapes, sizes, and colors. For Class 1 students, learning about birds can help them understand more about nature and the world around them.
-=======
+
 
 
 Dhivya
@@ -31,7 +30,7 @@ Mounika
 sivagama sundari
 Anusha
 
->>>>>>> e969d52f8a03d6ebf22c9813d649d0c886ee834e
+
 Naresh
 
 suji
