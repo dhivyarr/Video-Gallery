fa7e45d9599e03d25bf2426c7a0e08927ccb2605
Mounika Kema <mounika.k@tmachinesoftware.com>
2023-08-31 11:21:54 +0530

    para added
    Signed-off-by: Mounika Kema <mounika.k@tmachinesoftware.com>

--- Video_Gallery_Project/video_project_Backend/MOUNI.py
+++ Video_Gallery_Project/video_project_Backend/MOUNI.py
@@ -9,7 +9,7 @@ anusha
 siva
 
 
-
+Birds are animals that have feathers, wings, and beaks. They are warm-blooded and lay eggs. Birds are often found in the sky, trees, and on the ground, and they come in various shapes, sizes, and colors. For Class 1 students, learning about birds can help them understand more about nature and the world around them.
 Naresh
 
 suji
