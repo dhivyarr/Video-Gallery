03d68bd4e81cf892f5095552545477d60b45bfae
Dhivya R <dhivya.r@tmachinesoftware.com>
2023-08-31 10:45:03 +0530

    modify
    Signed-off-by: Dhivya R <dhivya.r@tmachinesoftware.com>

--- Video_Gallery_Project/video_project_Backend/MOUNI.py
+++ Video_Gallery_Project/video_project_Backend/MOUNI.py
@@ -10,16 +10,14 @@ siva
 
 
 
-<<<<<<< HEAD
-<<<<<<< HEAD
+
 Dhivya
-=======
+
 
 Mounika
 sivagama sundari
 Anusha
->>>>>>> 6fb131b90d35e0ab716afac019a0b4f62d1dd0f9
-=======
+
 Naresh
 
 suji
@@ -28,4 +26,4 @@ Mounika
 Sivagama sundari
 
 Dhivya
->>>>>>> cec26ed6c589d956bc06301849ee566802e899f1
+
